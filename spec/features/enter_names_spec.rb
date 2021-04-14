32784d176898f30b18bf478a2c01765bbd21cf87
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-04-14 20:30:22 +0100

    refactored by enabling sessions

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,12 @@
 feature 'enter_name' do
   scenario 'players can enter names and see those names on screen' do
     visit('/')
-    fill_in('player_1_name', with: 'finn')
-    fill_in('player_2_name', with: 'muhammad')
+    fill_in('player_1_name', with: 'player 1')
+    fill_in('player_2_name', with: 'player 2')
     click_button('Submit')
 
     save_and_open_page
 
-    expect(page).to have_content('finn vs. muhammad')
+    expect(page).to have_content('player 1 vs. player 2')
   end
 end
