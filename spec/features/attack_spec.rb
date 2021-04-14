1a0d083f6cd9f4948a52472a86a8e763fd65907f
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-04-14 22:56:12 +0100

    user story 4 - implementing hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -5,4 +5,12 @@ feature 'Attack' do
 
     expect(page).to have_content('muhammad attacked abdur')
   end
+
+  scenario 'player 1 attack reduces player 2 points by 10' do
+    sign_in_and_play
+    click_button('Attack')
+    click_button('Continue Playing')
+    expect(page).not_to have_content('abdur: 60HP')
+    expect(page).to have_content('abdur: 50HP')
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,32 @@
 require 'player'
 
 describe Player do
-  subject(:muhammad) { described_class.new('muhammad') }
+  subject(:muhammad)       { described_class.new('muhammad') }
+  subject(:abdur)          { described_class.new('abdur') }
+  let(:default_hp)         { described_class::DEFAULT_HP }
 
-  it 'has a name assigned to it' do
-    expect(muhammad.name).to eq('muhammad')
+  describe '#name' do
+    it 'has a name assigned to it' do
+      expect(muhammad.name).to eq('muhammad')
+    end
   end
 
+  describe '#hit_point' do
+    it 'each player has 60 points by default' do 
+      expect(muhammad.hit_points).to eq(default_hp)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces own hit points when attacked' do
+      expect { muhammad.receive_damage }.to change { muhammad.hit_points }.by(-10)
+    end
+  end
+
+  describe '#attack' do
+    it 'reduces the HP of player 2 after attack by player 1' do
+      expect{ muhammad.attack(abdur) }.to change { abdur.hit_points }.by(-10)
+    end
+  end
 end
+
