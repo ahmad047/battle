60d216a4da620f91ff876b6f467e97d8f8683d5e
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-04-14 20:51:07 +0100

    refactor tests by using web helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,11 +1,8 @@
 feature 'enter_name' do
   scenario 'players can enter names and see those names on screen' do
-    visit('/')
-    fill_in('player_1_name', with: 'player 1')
-    fill_in('player_2_name', with: 'player 2')
-    click_button('Submit')
+    sign_in_and_play
 
-    save_and_open_page
+    # save_and_open_page
 
     expect(page).to have_content('player 1 vs. player 2')
   end
